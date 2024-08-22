31aad5a800ad923f26ecfff8b8c542c45c4a88a4
Alistair Scott <ali.scott@gmail.com>
2024-08-22 12:21:09 +0100

    fix: prevent resolving dependencies multiple times
    This makes sure that modules that have already been resolved aren't resolved again. Previously, this wasn't passing all the previously resolved modules through the recursive calls which was causing the max recursion limit to be hit when it shouldn't have.

--- configstack/module.go
+++ configstack/module.go
@@ -380,7 +380,7 @@ func resolveExternalDependenciesForModules(moduleMap map[string]*TerraformModule
 	}
 
 	if len(allExternalDependencies) > 0 {
-		recursiveDependencies, err := resolveExternalDependenciesForModules(allExternalDependencies, moduleMap, recursionLevel+1, terragruntOptions, childTerragruntConfig)
+		recursiveDependencies, err := resolveExternalDependenciesForModules(allExternalDependencies, modulesToSkip, recursionLevel+1, terragruntOptions, childTerragruntConfig)
 		if err != nil {
 			return allExternalDependencies, err
 		}
